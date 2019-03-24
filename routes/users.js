f2bc57e258e8bb575e3e14f63049e27566c0916d
Victoria Buchanan <victoriabuchanan@pursuit.org>
2019-03-23 23:49:18 -0400

    user routes created

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,12 @@
 const express = require('express');
 const usersRouter = express.Router();
 const UsersService = require('../services/users');
+
+usersRouter.post('/', (req,res,next)=>{
+    const{userName, email, firstName, lastName, id} = req.body;
+
+    UsersService.creat(userName, email, firstName, lastName, id)
+    .then(data =>{
+        res.json({success: `Created New Username ${userName} with generated ID :${data.id}`})
+    })
+})
