9e67b6a28184c094d4cdb485a13260455473b873
Victoria Buchanan <victoriabuchanan@pursuit.org>
2019-03-24 12:35:42 -0400

    password removed

--- seed.sql
+++ seed.sql
@@ -7,7 +7,6 @@ CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   userName VARCHAR UNIQUE NOT NULL,
   email VARCHAR UNIQUE NOT NULL,
-  password VARCHAR NOT NULL,
   firstName VARCHAR NOT NULL,
   lastName VARCHAR NOT NULL, 
   location VARCHAR NOT NULL, 
@@ -24,7 +23,7 @@ CREATE TABLE restaurants (
   hometown_id INT REFERENCES cities (id) NOT NULL,
 );
 
-INSERT INTO users (userName, email, firstName, lastName, password) VALUES
+INSERT INTO users (userName, email, firstName, lastName) VALUES
 ('vvickyb', 'vickyb@gmail.com', 'victoria','buchanan', 12345678),
 ('rickybb', 'rickyb@gmail.com', 'ricky','tacos', 23456789),
 ('mikemyers', 'mikemyers@gmail.com', 'mike','myers', 'tacos123'),
