946bb4b49cc7475f1853dcfff8bf493301d434ab
Victoria Buchanan <victoriabuchanan@pursuit.org>
2019-03-30 10:49:30 -0400

    search update added and created was fixed

--- routes/search.js
+++ routes/search.js
@@ -3,14 +3,28 @@ const searchRouter = express.Router();
 const SearchService = require('../services/search');
 
 //create- POST
-usersRouter.post('/', (req,res,next)=>{
-    const{userName, id} = req.body;
+searchRouter.post('/search', (req,res,next)=>{
+    const{username, id} = req.body;
 
-    UsersService.create(userName, id)
+    SearchService.create(username, id)
     .then(data =>{
-        res.json({success: `found ${userName}`})
+        res.json({success: `found ${username}`})
     })
     .catch(err =>{
         next(err);
     })
 });
+
+//update- PUT
+searchRouter.put('/search', (req, res, next)=>{
+    const {username}= req.body
+    const {id}=req.params;
+
+    SearchService.update(id, username)
+    .then(data =>{
+        res.json({success: `Search for username ${userName} is being updated`})
+    })
+    .catch(err=>{
+        next(err);
+    })
+})
