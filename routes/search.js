d60f913b31f4092ea6b6ffaeae7ef6b627704ed7
Victoria Buchanan <victoriabuchanan@pursuit.org>
2019-03-30 10:45:50 -0400

    post created

--- routes/search.js
+++ routes/search.js
@@ -1,3 +1,16 @@
 const express = require('express');
 const searchRouter = express.Router();
 const SearchService = require('../services/search');
+
+//create- POST
+usersRouter.post('/', (req,res,next)=>{
+    const{userName, id} = req.body;
+
+    UsersService.create(userName, id)
+    .then(data =>{
+        res.json({success: `found ${userName}`})
+    })
+    .catch(err =>{
+        next(err);
+    })
+});
