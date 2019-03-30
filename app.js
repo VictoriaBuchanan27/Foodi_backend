910ebce95f5f8bac58fe40b422d93bfd873823c8
Victoria Buchanan <victoriabuchanan@pursuit.org>
2019-03-29 22:33:04 -0400

    app fixed needs more routes

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-const express= require('express');
+const express = require('express');
 const bodyParser = require('body-parser');// middeleware
-const cors=require('cors');//cors- can make request from local and remote server.
-const commentsRoutes= require('./comments')//to connect to file 
-const usersRoutes = require('./services/users')
+const cors =require('cors');//cors- can make request from local and remote server.
+
+//ROUTES
+const commentsRoutes= require('./routes/comments')//to connect to file 
+const usersRoutes = require('./routes/users')
+
 const app = express();// creating a instance of express
+
 const port = 3000;
 
  app.use(cors())
@@ -11,7 +15,6 @@ const port = 3000;
  app.use(bodyParser.urlencoded({extended:false}))//url is a func. recieving an obj
 
 
-const users=['billy','timmy','vicky']
 
 app.use('/comments',commentsRoutes)
 app.use('/users',usersRoutes)
